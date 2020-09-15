c18f07c1178af201227e0fd24f491270f88a9d50
gre-from-chertanovo <36995947+gre-from-chertanovo@users.noreply.github.com>
2020-09-16 01:21:58 +0300

    http requests fixed. need to dedicate them into separate .py file and revise quotes storage while calculating spread. Need to remove exact ticker nme in keys and substitue them with some proxy like XRP-USD-201225 => xrp_okx_dec or smth

--- data_collector_2020.py
+++ data_collector_2020.py
@@ -1,14 +1,44 @@
 import requests
 
 okexUrl='https://okex.com/api/futures/v3/instruments/'
-
+binanceUrl='https://www.binance.com/api/v3/depth'
+deriUrl='https://www.deribit.com/api/v1/public/getsummary'
+btmxUrl='https://www.bitmex.com/api/v1/orderBook/L2'
 
 def okexGet(okSym):
     okexMarkt=[]
     reqOkex = requests.get(str(okexUrl+'/'+okSym+'/book'))
     okexMarkt.append(reqOkex.json()['bids'][0][0])
     okexMarkt.append(reqOkex.json()['asks'][0][0])
+    okexMarkt=list(map(float, okexMarkt))
     return(okexMarkt)
-print(okexGet('BTC-USD-201225'))
+print(okexGet('EOS-USD-201225'))
 
+def binanceGet(binSym):
+    binanceParams={'symbol':binSym, 'limit':5}
+    binanceMarkt = []
+    reqBinance = requests.get(binanceUrl, binanceParams)
+    binanceMarkt.append(reqBinance.json()['bids'][0][0])
+    binanceMarkt.append(reqBinance.json()['asks'][0][0])
+    binanceMarkt = list(map(float, binanceMarkt))
+    return(binanceMarkt)
+#print(binanceGet('ETHBTC'))
+print((okexGet('BTC-USD-201225')[0])*(binanceGet('ETHBTC')[0]))
 
+def deriGet(derSym):
+    deriMarkt=[]
+    deriParams={'instrument': derSym}
+    reqDeri=requests.get(deriUrl, deriParams)
+    deriMarkt.append(reqDeri.json()['result']['bidPrice'])
+    deriMarkt.append(reqDeri.json()['result']['askPrice'])
+    return(deriMarkt)
+
+def btmxGet(btxSym):
+    btmxMarkt = []
+    btmxParams = {'symbol': btxSym, 'depth':'1'}
+    reqBtmx = requests.get(btmxUrl, btmxParams)
+    btmxMarkt.append(reqBtmx.json()[1]['price'])
+    btmxMarkt.append(reqBtmx.json()[0]['price'])
+    return(btmxMarkt)
+
+print(btmxGet('XBTU20'))
--- fut_getter_telesram.py
+++ fut_getter_telesram.py
@@ -1,12 +1,12 @@
 import requests
 import time
-import socks
-import socket
+#import socks
+#import socket
 
-ip='167.99.139.100'
-port = 1080
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, ip, port)
-socket.socket = socks.socksocket
+#ip='167.99.139.100'
+#port = 1080
+#socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, ip, port)
+#socket.socket = socks.socksocket
 
 url = "https://api.telegram.org/bot543539841:AAHDUPtmz1UHkj4UX7GvtrZoxlmmHI1Us0M/"
 #proxiesDick = {'socks':'tglive:tglive1@socks5://socks5.gq:1080'}
@@ -39,19 +39,28 @@ update_id = last_update(get_updates_json(url))['update_id']
 #offset = last_update(get_updates_json(url))['update_id']
 #telega_params={'offset': offset}
 
-okexUrl='https://www.okex.com/api/v1/future_ticker.do'
+okexUrl='https://okex.com/api/futures/v3/instruments/'
 deriUrl='https://www.deribit.com/api/v1/public/getsummary'
-btmxUrl='https://www.bitmex.com/api/v1/quote'
-deriSym = 'BTC-28DEC18'
-btmxSym = ['XBTZ18', 'ETHZ18', 'LTCZ18', 'BCHZ18', 'XRPZ18', 'EOSZ18']
-okexSym = ['btc_usd', 'eth_usd', 'ltc_usd', 'bch_usd', 'xrp_usd', 'eos_usd']
+btmxUrl='https://www.bitmex.com/api/v1/orderBook/L2'
+deriSym = 'BTC-25DEC20'
+btmxSym = ['XBTZ20', 'ETHZ20', 'LTCZ20', 'BCHZ20', 'XRPZ20', 'EOSZ20']
+okexSym = ['BTC-USD-201225', 'ETH-USD-201225', 'LTC-USD-201225', 'BCH-USD-201225', 'XRP-USD-201225', 'EOS-USD-201225']
+
+
+#def okexGet(okSym):
+#    okexMarkt=[]
+#    okexParams = {'symbol': okSym, 'contract_type': 'quarter'}
+#    reqOkex = requests.get(okexUrl, okexParams)
+#    okexMarkt.append(reqOkex.json()['ticker']['buy'])
+#    okexMarkt.append(reqOkex.json()['ticker']['sell'])
+#    return(okexMarkt)
 
 def okexGet(okSym):
     okexMarkt=[]
-    okexParams = {'symbol': okSym, 'contract_type': 'quarter'}
-    reqOkex = requests.get(okexUrl, okexParams)
-    okexMarkt.append(reqOkex.json()['ticker']['buy'])
-    okexMarkt.append(reqOkex.json()['ticker']['sell'])
+    reqOkex = requests.get(str(okexUrl+'/'+okSym+'/book'))
+    okexMarkt.append(reqOkex.json()['bids'][0][0])
+    okexMarkt.append(reqOkex.json()['asks'][0][0])
+    okexMarkt=list(map(float, okexMarkt))
     return(okexMarkt)
 
 def deriGet(derSym):
@@ -78,6 +87,8 @@ def btmxGet(btxSym):
     btmxMarkt.append(reqBtmx.json()[0]['price'])
     return(btmxMarkt)
 
+
+
 while True:
     if update_id == last_update(get_updates_json(url))['update_id']:
 
@@ -85,24 +96,24 @@ while True:
         bitmexQ = {k: btmxGet(k) for k in btmxSym}
         okexQ = {k: okexGet(k) for k in okexSym}
 
-        basisBTC = {'btc okex': [(okexQ['btc_usd'][0] - deriQ[1]), (okexQ['btc_usd'][1] - deriQ[0])],
-                    'btc btmx': [(bitmexQ['XBTM18'][0] - deriQ[1]), (bitmexQ['XBTM18'][1] - deriQ[0])]}
-        basis = {'ltc': [2 * (okexQ['ltc_usd'][0] / (bitmexQ['XBTM18'][1]) - (bitmexQ['LTCM18'][1])) / (
-                    bitmexQ['LTCM18'][0] + bitmexQ['LTCM18'][1]),
-                         2 * (okexQ['ltc_usd'][1] / (bitmexQ['XBTM18'][0]) - (bitmexQ['LTCM18'][0])) / (
-                                     bitmexQ['LTCM18'][0] + bitmexQ['LTCM18'][1])]}
-        basis['eth'] = [2 * (okexQ['eth_usd'][0] / (bitmexQ['XBTM18'][1]) - (bitmexQ['ETHM18'][1])) / (
-                    bitmexQ['ETHM18'][0] + bitmexQ['ETHM18'][1]),
-                        2 * (okexQ['eth_usd'][1] / (bitmexQ['XBTM18'][0]) - (bitmexQ['ETHM18'][0])) / (
-                                    bitmexQ['ETHM18'][0] + bitmexQ['ETHM18'][1])]
-        basis['bch'] = [2 * (okexQ['bch_usd'][0] / (bitmexQ['XBTM18'][1]) - (bitmexQ['BCHM18'][1])) / (
-                    bitmexQ['BCHM18'][0] + bitmexQ['BCHM18'][1]),
-                        2 * (okexQ['bch_usd'][1] / (bitmexQ['XBTM18'][0]) - (bitmexQ['BCHM18'][0])) / (
-                                    bitmexQ['BCHM18'][0] + bitmexQ['BCHM18'][1])]
-        basis['xrp'] = [2 * (okexQ['xrp_usd'][0] / (bitmexQ['XBTM18'][1]) - (bitmexQ['XRPM18'][1])) / (
-                    bitmexQ['XRPM18'][0] + bitmexQ['XRPM18'][1]),
-                        2 * (okexQ['xrp_usd'][1] / (bitmexQ['XBTM18'][0]) - (bitmexQ['XRPM18'][0])) / (
-                                    bitmexQ['XRPM18'][0] + bitmexQ['XRPM18'][1])]
+        basisBTC = {'btc okex': [(okexQ['BTC-USD-201225'][0] - deriQ[1]), (okexQ['BTC-USD-201225'][1] - deriQ[0])],
+                    'btc btmx': [(bitmexQ['XBTZ20'][0] - deriQ[1]), (bitmexQ['XBTZ20'][1] - deriQ[0])]}
+        basis = {'ltc': [2 * (okexQ['LTC-USD-201225'][0] / (bitmexQ['XBTZ20'][1]) - (bitmexQ['LTCZ20'][1])) / (
+                    bitmexQ['LTCZ20'][0] + bitmexQ['LTCZ20'][1]),
+                         2 * (okexQ['LTC-USD-201225'][1] / (bitmexQ['XBTZ20'][0]) - (bitmexQ['LTCZ20'][0])) / (
+                                     bitmexQ['LTCZ20'][0] + bitmexQ['LTCZ20'][1])]}
+        basis['eth'] = [2 * (okexQ['ETH-USD-201225'][0] / (bitmexQ['XBTZ20'][1]) - (bitmexQ['ETHZ20'][1])) / (
+                    bitmexQ['ETHZ20'][0] + bitmexQ['ETHZ20'][1]),
+                        2 * (okexQ['ETH-USD-201225'][1] / (bitmexQ['XBTZ20'][0]) - (bitmexQ['ETHZ20'][0])) / (
+                                    bitmexQ['ETHZ20'][0] + bitmexQ['ETHZ20'][1])]
+        basis['bch'] = [2 * (okexQ['BCH-USD-201225'][0] / (bitmexQ['XBTZ20'][1]) - (bitmexQ['BCHZ20'][1])) / (
+                    bitmexQ['BCHZ20'][0] + bitmexQ['BCHZ20'][1]),
+                        2 * (okexQ['BCH-USD-201225'][1] / (bitmexQ['XBTZ20'][0]) - (bitmexQ['BCHZ20'][0])) / (
+                                    bitmexQ['BCHZ20'][0] + bitmexQ['BCHZ20'][1])]
+        basis['xrp'] = [2 * (okexQ['XRP-USD-201225'][0] / (bitmexQ['XBTZ20'][1]) - (bitmexQ['XRPZ20'][1])) / (
+                    bitmexQ['XRPZ20'][0] + bitmexQ['XRPZ20'][1]),
+                        2 * (okexQ['XRP-USD-201225'][1] / (bitmexQ['XBTZ20'][0]) - (bitmexQ['XRPZ20'][0])) / (
+                                    bitmexQ['XRPZ20'][0] + bitmexQ['XRPZ20'][1])]
 
         mess=str('BTC:\nOkex - Deribit (%.2f %.2f)\nBitmex - Deribit (%.2f %.2f)\nIchpochmak:\nLTC: (%.4f %.4f)\nETH: (%.4f %.4f)\nBCH: (%.4f %.4f)\nXRP: (%.4f %.4f)' %(basisBTC['btc okex'][0],basisBTC['btc okex'][1],basisBTC['btc btmx'][0],basisBTC['btc btmx'][1], basis['ltc'][0],basis['ltc'][1],basis['eth'][0],basis['eth'][1],basis['bch'][0],basis['bch'][1], basis['xrp'][0],basis['xrp'][1]))
         send_mess(get_chat_id(last_update(get_updates_json(url))), mess)
